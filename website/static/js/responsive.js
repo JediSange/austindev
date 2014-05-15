14491bcc009b826f14bb9bdd1987551ca8bcc32f
JediSange <OniKami@gmail.com>
2014-05-15 03:46:05 +0000

    Spaces ugh

--- website/static/js/responsive.js
+++ website/static/js/responsive.js
@@ -1,9 +1,9 @@
 "use strict";
 
 $(document).ready(function() {
-	$('.mobile-nav').click(function() {
-		$('.main-container').toggleClass('menu-active');
-		$('.mobile-nav').toggleClass('menu-active');
-		$('.side-nav').toggleClass('menu-active');
-	});
+  $('.mobile-nav').click(function() {
+    $('.main-container').toggleClass('menu-active');
+    $('.mobile-nav').toggleClass('menu-active');
+    $('.side-nav').toggleClass('menu-active');
+  });
 });
